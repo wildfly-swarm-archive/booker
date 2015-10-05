ef34d6b3c8ca51bb3e6602e5ac0dc928e69a8e1b
Bob McWhirter <bob@mcwhirter.org>
2015-10-05 14:06:41 -0400

    If a user is authenticated, ensure we bounce through keycloak.updateToken() to ensure the token is refreshed in case it expired while John made coffee.

--- web-client/src/main/webapp/js/ribbon.js
+++ web-client/src/main/webapp/js/ribbon.js
@@ -97,20 +97,40 @@ Ribbon.ajax = function(serviceName, url, settings) {
   }
 
   var headers = settings.headers || {};
-
-  if ( keycloak.token ) {
-    headers.Authorization = 'Bearer ' + keycloak.token
-  }
-
   settings.url = '//' + allServers[0] + url;
   settings.headers = headers;
 
-  if ( allServers.length > 0 ) {
-    return $.ajax( settings );
+  if ( allServers.length < 1 ) {
+    var deferred = $.Deferred();
+    return deferred.reject();
+  }
+
+  if ( ! keycloak.authenticated ) {
+    console.log( "not authenticated" );
+    return Ribbon._doRequest( settings, false );
   }
 
   var deferred = $.Deferred();
-  return deferred.reject();
+
+  keycloak.updateToken(30).success( function() {
+    console.log( "refreshed" );
+    Ribbon._doRequest( settings, true ).then( function(result) {
+      deferred.resolve(result);
+    })
+    //deferred.resolve( Ribbon._doRequest( settings, true ) );
+  }).error( function() {
+    console.log( "Unable to update token" );
+    deferred.reject();
+  })
+
+  return deferred;
+}
+
+Ribbon._doRequest = function(settings, useToken) {
+  if ( useToken ) {
+    settings.headers.Authorization = 'Bearer ' + keycloak.token;
+  }
+  return $.ajax( settings );
 }
 
 Ribbon.getJSON = function(serviceName, url, data) {
