44d46aacf82196183307d645f7ffe7f7dfb31bfe
Lance Ball <lball@redhat.com>
2015-11-02 13:08:13 -0500

    Convert webapp component of the application to use ribbon-webapp.

---
+++ web-client/src/main/java/org/wildfly/swarm/booker/Main.java
@@ -0,0 +1,18 @@
+package org.wildfly.swarm.booker;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.wildfly.swarm.container.Container;
+import org.wildfly.swarm.undertow.WARArchive;
+
+/**
+ * @author Lance Ball
+ */
+public class Main {
+    public static void main(String... args) throws Exception {
+        Container container = new Container();
+        container.start();
+        WARArchive war = ShrinkWrap.create(WARArchive.class);
+        war.staticContent();
+        container.deploy(war);
+    }
+}
--- web-client/src/main/resources/js/topology.js
+++ web-client/src/main/resources/js/topology.js
@@ -1,10 +1,9 @@
-
 Booker.Actions.Topology = Reflux.createAction();
 
 Booker.State.Topology = Reflux.createStore({
   init: function() {
     this.listenTo( Booker.Actions.Topology, this.output );
-    this.sse = new EventSource( "/system/ribbon/stream" );
+    this.sse = new EventSource( "/ribbon/system/stream" );
     this.sse.onmessage = Booker.Actions.Topology;
     this.sse.onerror = function(e) {
       console.log( "SSE error", e );
@@ -83,67 +82,3 @@ Booker.TopologyService = React.createClass({
     )
   }
 });
-
-
-Ribbon = {};
-
-Ribbon.ajax = function(serviceName, url, settings) {
-  var allServers = Booker.State.Topology.servers( serviceName );
-
-  console.log( "servers for [" + serviceName + "]", allServers );
-
-  if ( ! settings ) {
-    settings = {};
-  }
-
-  var headers = settings.headers || {};
-  settings.url = '//' + allServers[0] + url;
-  settings.headers = headers;
-
-  if ( allServers.length < 1 ) {
-    var deferred = $.Deferred();
-    return deferred.reject();
-  }
-
-  if ( ! keycloak.authenticated ) {
-    console.log( "not authenticated" );
-    return Ribbon._doRequest( settings, false );
-  }
-
-  var deferred = $.Deferred();
-
-  keycloak.updateToken(30).success( function() {
-    console.log( "refreshed" );
-    Ribbon._doRequest( settings, true ).then( function(result) {
-      deferred.resolve(result);
-    })
-    //deferred.resolve( Ribbon._doRequest( settings, true ) );
-  }).error( function() {
-    console.log( "Unable to update token" );
-    deferred.reject();
-  })
-
-  return deferred;
-}
-
-Ribbon._doRequest = function(settings, useToken) {
-  if ( useToken ) {
-    settings.headers.Authorization = 'Bearer ' + keycloak.token;
-  }
-  return $.ajax( settings );
-}
-
-Ribbon.getJSON = function(serviceName, url, data) {
-  return Ribbon.ajax( serviceName, url, {
-    dataType: 'json',
-    data: data,
-  })
-}
-
-Ribbon.postJSON = function(serviceName, url, data) {
-  return Ribbon.ajax( serviceName, url, {
-    dataType: 'json',
-    method: 'POST',
-    data: data,
-  })
-}
