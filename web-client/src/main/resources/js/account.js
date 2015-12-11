f583a6ce6d0c3bd131c8d480e632080bc27d447c
Lance Ball <lball@redhat.com>
2015-12-11 11:16:06 -0500

    Add a little something to the Account page. Display keycloak user info.

--- web-client/src/main/resources/js/account.js
+++ web-client/src/main/resources/js/account.js
@@ -1,8 +1,24 @@
-
 Booker.Account = React.createClass({
+  mixins: [Reflux.connect(Booker.State.UserInfo,"user")],
+  getInitialState: function() {
+    return {
+      user: {
+        name: 'Booker User',
+      }
+    };
+  },
+
   render: function() {
     return (
-      <h1>Your Account</h1>
+      <div className='account-panel'>
+        <h2> Your Account</h2>
+        <div className='user-info'>
+          <ul>
+        <li>Name: {this.state.user.name}</li>
+        <li>Username: {this.state.user.preferred_username}</li>
+          </ul>
+          </div>
+      </div>
     );
   }
 });
