e8b6a80c328cd9de2ecce2279a31964b86e0f201
jclingan <jclingan@redhat.com>
2015-12-19 15:22:59 -0600

    Fixed ContainerUtils to be compatible with updated LoggingFraction that uses new Config API

--- common/src/main/java/org/wildfly/swarm/booker/common/ContainerUtils.java
+++ common/src/main/java/org/wildfly/swarm/booker/common/ContainerUtils.java
@@ -3,19 +3,17 @@ package org.wildfly.swarm.booker.common;
 import org.wildfly.swarm.config.logging.Logger;
 import org.wildfly.swarm.jgroups.JGroupsFraction;
 import org.wildfly.swarm.logging.LoggingFraction;
+import org.wildfly.swarm.config.logging.Level;
 
 public class ContainerUtils {
 
     public static LoggingFraction loggingFraction() {
-        LoggingFraction fraction = new LoggingFraction()
-                .defaultColorFormatter()
-                .consoleHandler(LoggingFraction.Level.TRACE, LoggingFraction.COLOR_PATTERN)
-                .rootLogger(LoggingFraction.Level.INFO, LoggingFraction.CONSOLE);
+        LoggingFraction fraction = LoggingFraction.createDefaultLoggingFraction();
 
         fraction.logger(new Logger("org.openshift.ping")
-                .level(LoggingFraction.Level.TRACE));
+                .level(Level.TRACE));
         fraction.logger(new Logger("org.jgroups.protocols.openshift")
-                .level(LoggingFraction.Level.TRACE));
+                .level(Level.TRACE));
 
         return fraction;
     }
